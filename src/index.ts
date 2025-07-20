5bcd78214d0861c516b2f2eeb390e494273d4431
Israel de Souza <israellimas@hotmail.com>
2025-07-20 08:13:12 -0300

    config server to render interface

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import express from 'express'
-import shortRoute from './routes/shortRoutes'
+import express from "express";
+import path from "path";
+import shortRoute from "./routes/shortRoutes";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use('/api/short', shortRoute)
+app.use("/api/short", shortRoute);
 
-
-
-
-const PORT:number = 3000;
+const PORT: number = 3000;
 app.listen(PORT, () => {
-    console.log(`Server listen in port http://localhost:${PORT}`)
-})
+  console.log(`Server listen in port http://localhost:${PORT}`);
+});
