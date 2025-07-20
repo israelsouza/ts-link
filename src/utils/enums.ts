7d8126b6ead6bcbedce357af7ddea3b965b8c530
Israel de Souza <israellimas@hotmail.com>
2025-07-19 22:00:37 -0300

    add message for success case

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -9,5 +9,6 @@ export enum Messages {
     URL_REQUIRED = 'Insira a URL para prosseguir.',
     INVALID_URL = 'Url enviada é inválida.',
     URL_EXISTS = 'Url enviada já foi cadastrada.',
-    INTERNAL_SERVER_ERROR = 'Erro interno ao criar o código, tente novamente.'
+    INTERNAL_SERVER_ERROR = 'Erro interno ao criar o código, tente novamente.',
+    URL_CREATED = 'URL criada com sucesso!'
 }
